f36c44e411b9c3caab4c4e4db8fe1705dd3b8b5d
Doron Marcus <marcus.doron@gmail.com>
2016-01-14 01:50:35 +0200

    Allow cross origin

--- gs-rest-service/src/main/java/controller/VocabolaryController.java
+++ gs-rest-service/src/main/java/controller/VocabolaryController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,7 +36,7 @@ public class VocabolaryController {
 		return "ok";
 
 	}
-
+	@CrossOrigin
 	@RequestMapping("/getWords")
 	public VocabularyWordContainer getCustomers(
 			@RequestParam(value = "wordGroup", defaultValue = "WeekDays") String wordGroup) {
